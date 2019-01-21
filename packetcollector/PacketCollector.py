ce67c4a8778470c4c08dd457312167a9b3c67587
johnsonjulian38 <35655618+johnsonjulian38@users.noreply.github.com>
2019-01-20 20:20:56 -1000

    cleaning up
    Outputs Scapy OSErrors to the capture function.

--- packetcollector/PacketCollector.py
+++ packetcollector/PacketCollector.py
@@ -7,28 +7,33 @@ class PacketCollector:
     def __init__(self, timeout=10, interface=None, out_dir=None):
         self.timeout = timeout
         self.interface = interface
-        
-        if out_dir == None:
+
+        if out_dir is None:
             self.out_dir = os.getcwd()
         else:
             self.out_dir = out_dir
-        
+
         self.packets = None
 
     def capture(self):
+        # starts sniffing packets. If the device is incorrectly named, it will show
         try:
             self.packets = sniff(iface=self.interface, timeout=self.timeout)
-        except ValueError:
-            print("Could not capture on {}".format(self.interface))
-        
+        except OSError as e:
+            print(e)
+
     def save(self):
-        filename = 	self.out_dir + "/" + self._get_filename()
+        # checks if pcap directory exists. Creates one if it isn't
+        if not os.path.exists(self.out_dir):
+            os.makedirs(self.out_dir)
+
+        filename = self.out_dir + "/" + self._get_filename()
         wrpcap(filename, self.packets)
 
     def _get_filename(self):
-	    return str(time.time()) + ".pcap" 
-	
-	    #Stream allows for multiple captures to occur and to continue to save to the same folder. 0 is infinite
+        return str(time.time()) + ".pcap"
+
+    # allows for specific number of captures. Used for debugging. stream(0) is a constant capture and save.
     def stream(self, x):
         if x is 0:
             while True:
