fc661f29eee0501ad2e4cee8e6c50a892f868703
Kyle Topasna <kyle.topasna@gmail.com>
2019-02-19 21:24:34 -0900

    Added function to find packets in time window
    Added a function to find packets in a specified time window. The
    current function doesn't handle the remain left over packets from
    the PCAP iterable.

--- utils/PacketReplay.py
+++ utils/PacketReplay.py
@@ -1,10 +1,39 @@
 from scapy.all import *
 import argparse
+import datetime
 
-pcap = 
 
-def breakout_pcap(pcap_file):
+
+def batch_generator(iterable, window)= 5:
+    packet_out = []
+    count
+    source_iterable = iter(iterable)
+    while True:
+        batch_iterable
+
+def breakout_pcap(pcap_file, time_window=5):
+
+    packet_out = []
+    count = 0
+    try:
+        for packet in pcap_file:
+            packet_out.append(packet)
+            time_diff = packet.time - packet_out[0].time
+            if time_diff > time_window:
+                count += 1
+                packet_out.pop()
+                print(count)
+                wrpcap("packet_replay_test_" + str(count) + ".pcap", packet_out)
+                packet_out = []
+                
+    except:
+        count += 1
+        print(count)
+        wrpcap("packet_replay_test_" + str(count) + ".pcap", packet_out)
+        packet_out = []
+
     
+        
 
 if __name__ == "__main__":
 
@@ -17,6 +46,7 @@ if __name__ == "__main__":
     infile =  ga.infile
     try:
         pkt_reader = PcapReader(infile)
+        breakout_pcap(pkt_reader)
     except IOError:
-        print "Failed reading file %s contents" % infile
+        print("Failed reading file %s contents" % infile)
         sys.exit(1)
