49ea5e8e2c6cbf560c87c00da6e6843c3e6160d2
Kyle Topasna <kyle.topasna@gmail.com>
2019-02-20 07:48:11 -0900

    Updated signature to take outfile name

--- utils/PacketReplay.py
+++ utils/PacketReplay.py
@@ -2,16 +2,24 @@ from scapy.all import *
 import argparse
 import datetime
 
+count = 0
 
-
-def batch_generator(iterable, window)= 5:
+def batch_generator(pcap_file, window=5):
     packet_out = []
-    count
-    source_iterable = iter(iterable)
-    while True:
-        batch_iterable
 
-def breakout_pcap(pcap_file, time_window=5):
+    source_iterable = iter(pcap_file)
+    while True:
+        for packet in source_iterable:
+            packet_out.append(packet)
+            time_diff = packet.time - packet_out[0].time
+            if time_diff > time_window:
+                count += 1
+                last_packet = packet_out.pop()
+                batch_iter = iter(packet_out)
+                packet_out = [last_packet]
+                yield chain([batch_iter.next()])
+
+def breakout_pcap(pcap_file, out_file_name, time_window=5, ):
 
     packet_out = []
     count = 0
@@ -21,15 +29,15 @@ def breakout_pcap(pcap_file, time_window=5):
             time_diff = packet.time - packet_out[0].time
             if time_diff > time_window:
                 count += 1
-                packet_out.pop()
+                last_packet = packet_out.pop()
                 print(count)
-                wrpcap("packet_replay_test_" + str(count) + ".pcap", packet_out)
-                packet_out = []
+                wrpcap(out_file_name + str(count) + ".pcap", packet_out)
+                packet_out = [last_packet]
                 
     except:
         count += 1
         print(count)
-        wrpcap("packet_replay_test_" + str(count) + ".pcap", packet_out)
+        wrpcap(out_file_name + str(count) + ".pcap", packet_out)
         packet_out = []
 
     
@@ -46,7 +54,7 @@ if __name__ == "__main__":
     infile =  ga.infile
     try:
         pkt_reader = PcapReader(infile)
-        breakout_pcap(pkt_reader)
+        breakout_pcap(pkt_reader, "test_out_file", 5)
     except IOError:
         print("Failed reading file %s contents" % infile)
         sys.exit(1)
